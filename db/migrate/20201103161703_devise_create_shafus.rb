f36b2f39737013793fd0961868920cffefc19cf6
kansei-bit <kansei1001@gmail.com>
2020-11-04 01:24:10 +0900

    shafuテーブルのマイグレーションファイルの記述

--- db/migrate/20201103161703_devise_create_shafus.rb
+++ db/migrate/20201103161703_devise_create_shafus.rb
@@ -6,6 +6,11 @@ class DeviseCreateShafus < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :lname, null: false, default: ""
+      t.string :fname, null: false, default: ""
+      t.string :lname_katakana, null: false, default: ""
+      t.string :fname_katakana, null: false, default: ""
+      t.date :birthday, null: false
 
       ## Recoverable
       t.string   :reset_password_token
