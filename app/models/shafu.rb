81fec564a6a84407b9a1f3a6ac93f274931a8675
kansei-bit <kansei1001@gmail.com>
2020-11-04 01:38:58 +0900

    shafuモデルにバリデーション設定

--- app/models/shafu.rb
+++ app/models/shafu.rb
@@ -1,6 +1,18 @@
 class Shafu < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  zenkaku = /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/
+  zenkaku_katakana = /\A[ァ-ヶー－]+\z/
+  validates :birthday, presence: true
+  validates :password, format: { with: PASSWORD_REGEX, message: 'Include both letters and numbers' }
+  with_options presence: true, format: { with: zenkaku, message: 'Full-width characters' } do
+    validates :lname
+    validates :fname
+  end
+  with_options presence: true, format: { with: zenkaku_katakana, message: 'Full-width katakana characters' } do
+    validates :lname_katakana
+    validates :fname_katakana
+  end
 end
